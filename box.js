994d92d2d282cb46f498c77e5e0cec38e7eccef8
sarthak0711 <68766980+sarthak0711@users.noreply.github.com>
2020-08-18 09:19:27 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,17 @@
+class BoxDrop  {
+
+    constructor(x,y,width,height){
+        var options = {'restitution':0}
+    
+    this.body=Bodies.rectangle(x , y,width,height,options);
+    this.width = width
+    this.height = height
+
+    World.add(world,this.body);
+    } 
+    display(){
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+    }
+
+}
